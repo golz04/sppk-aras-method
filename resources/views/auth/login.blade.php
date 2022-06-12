b69dec736858458f41cb64fac9df9dacb9b80319
golz04 <muhammadhaidar862@gmail.com>
2022-06-12 14:58:12 +0700

    setup vendor + login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -8,7 +8,6 @@
 
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
-
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
@@ -53,4 +52,90 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="apple-touch-icon" sizes="76x76" href="{{asset('assets/img/apple-icon.png')}}" />
+    <link rel="icon" type="image/png" href="{{asset('assets/img/favicon.png')}}" />
+    <title>Aras Method</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="{{asset('assets/css/nucleo-icons.css')}}" rel="stylesheet" />
+    <link rel="stylesheet" href="{{asset('assets/css/nucleo-svg.css')}}" />
+    <link rel="stylesheet" href="{{asset('assets/css/styles.css?v=1.0.1')}}" />
+  </head>
+
+  <body class="m-0 font-sans antialiased font-normal bg-white text-start text-size-base leading-default text-slate-500">
+    <main class="mt-0 transition-all duration-200 ease-soft-in-out">
+        <section>
+            <div class="relative flex items-center p-0 overflow-hidden bg-center bg-cover min-h-75-screen">
+                <div class="container z-10">
+                    <div class="flex flex-wrap mt-0 -mx-3">
+                        <div class="flex flex-col w-full max-w-full px-3 mx-auto md:flex-0 shrink-0 md:w-6/12 lg:w-5/12 xl:w-4/12">
+                            <div class="relative flex flex-col min-w-0 mt-32 break-words bg-transparent border-0 shadow-none rounded-2xl bg-clip-border">
+                                <div class="p-6 pb-0 mb-0 bg-transparent border-b-0 rounded-t-2xl">
+                                    <h3 class="relative z-10 font-bold text-transparent bg-gradient-cyan bg-clip-text">Selamat Datang Admin</h3>
+                                    <p class="mb-0">Silahkan isi email dan password untuk login</p>
+                                </div>
+                                <div class="flex-auto p-6">
+                                    <!-- Session Status -->
+                                    <x-auth-session-status class="mb-4" :status="session('status')" />
+                                    <!-- Validation Errors -->
+                                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <label class="mb-2 ml-1 font-bold text-size-xs text-slate-700">Email</label>
+                                        <div class="mb-4">
+                                            <input type="email" name="email" value="{{old('email')}}" class="focus:shadow-soft-primary-outline text-size-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow" placeholder="Email" aria-label="Email" aria-describedby="email-addon" />
+                                        </div>
+                                        <label class="mb-2 ml-1 font-bold text-size-xs text-slate-700">Password</label>
+                                        <div class="mb-4">
+                                            <input type="password" name="password" value="{{old('password')}}" class="focus:shadow-soft-primary-outline text-size-sm leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-2 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:outline-none focus:transition-shadow" placeholder="Password" aria-label="Password" aria-describedby="password-addon" />
+                                        </div>
+                                        <div class="min-h-6 mb-0.5 block pl-12">
+                                            <input id="rememberMe" class="mt-0.54 rounded-10 duration-250 ease-soft-in-out after:rounded-circle after:shadow-soft-2xl after:duration-250 checked:after:translate-x-5.25 h-5-em relative float-left -ml-12 w-10 cursor-pointer appearance-none border border-solid border-gray-200 bg-slate-800/10 bg-none bg-contain bg-left bg-no-repeat align-top transition-all after:absolute after:top-px after:h-4 after:w-4 after:translate-x-px after:bg-white after:content-[''] checked:border-slate-800/95 checked:bg-slate-800/95 checked:bg-none checked:bg-right" type="checkbox" checked="" />
+                                            <label class="mb-2 ml-1 font-normal cursor-pointer select-none text-size-sm text-slate-700" for="rememberMe">Ingat Saya</label>
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit" class="inline-block w-full px-6 py-3 mt-6 mb-0 font-bold text-center text-white uppercase align-middle transition-all bg-transparent border-0 rounded-lg cursor-pointer shadow-soft-md bg-x-25 bg-150 leading-pro text-size-xs ease-soft-in tracking-tight-soft bg-gradient-cyan hover:scale-102 hover:shadow-soft-xs active:opacity-85">Login</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    <div class="w-full max-w-full px-3 lg:flex-0 shrink-0 md:w-6/12">
+                        <div class="absolute top-0 hidden w-3/5 h-full -mr-32 overflow-hidden -skew-x-10 -right-40 rounded-bl-xl md:block">
+                            <div class="absolute inset-x-0 top-0 z-0 h-full -ml-16 bg-cover skew-x-10" style="background-image: url('../assets/img/curved-images/curved6.jpg')"></div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer class="py-12">
+      <div class="container">
+        <div class="flex flex-wrap -mx-3">
+          <div class="w-8/12 max-w-full px-3 mx-auto mt-1 text-center flex-0">
+            <p class="mb-0 text-slate-400">
+              Copyright ©
+              <script>
+                document.write(new Date().getFullYear());
+              </script>
+              Aldi - Amiroh - Haidar Teams.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </body>
+
+  <script src="{{asset('assets/js/perfect-scrollbar.js')}}"></script>
+  <script src="{{asset('assets/js/soft-ui-dashboard-tailwind.js?v=1.0.1')}}"></script>
+</html>
